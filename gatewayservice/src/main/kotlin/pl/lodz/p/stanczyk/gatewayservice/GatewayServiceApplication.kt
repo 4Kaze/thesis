3440dedd00b4ae439b260ef4909726781ee876e0
4Kaze <32jacke32@gmail.com>
2021-01-26 21:56:08 +0100

    fix properties scan

--- gatewayservice/src/main/kotlin/pl/lodz/p/stanczyk/gatewayservice/GatewayServiceApplication.kt
+++ gatewayservice/src/main/kotlin/pl/lodz/p/stanczyk/gatewayservice/GatewayServiceApplication.kt
@@ -1,11 +1,15 @@
 package pl.lodz.p.stanczyk.gatewayservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
+import pl.lodz.p.stanczyk.gatewayservice.config.SSLConfig
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableConfigurationProperties(SSLConfig::class)
 class GatewayServiceApplication
 
 fun main(args: Array<String>) {
