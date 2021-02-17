083ad9dc36b94eac443e19138757b43203645a3e
4Kaze <32jacke32@gmail.com>
2021-02-17 22:24:27 +0100

    downgrade keycloak adapter

--- gatewayservice/build.gradle.kts
+++ gatewayservice/build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation(platform("org.keycloak.bom:keycloak-adapter-bom:12.0.1"))
+    implementation(platform("org.keycloak.bom:keycloak-adapter-bom:6.0.1"))
     implementation("org.keycloak:keycloak-spring-boot-starter")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
